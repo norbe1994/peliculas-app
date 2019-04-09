0fe2c488bdef043607aa7337d5bf96cdd1e726f3
Norberto Caceres <norbe1994@yahoo.com>
2019-04-09 11:06:33 -0500

    crear home y navbar components y rutaje basico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { HomeComponent } from './components/home/home.component'
+import { BuscarComponent } from './components/buscar/buscar.component'
+import { Routes, RouterModule } from '@angular/router'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'buscar', component: BuscarComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,16 @@ import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module'
 // componentes
 import { AppComponent } from './app.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { HomeComponent } from './components/home/home.component'
+import { BuscarComponent } from './components/buscar/buscar.component'
 // servicios
 import { PeliculasService } from './services/peliculas.service'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientJsonpModule,
-    HttpClientModule,
-    AppRoutingModule,
-  ],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, BuscarComponent],
+  imports: [BrowserModule, HttpClientJsonpModule, HttpClientModule, AppRoutingModule],
   providers: [PeliculasService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
